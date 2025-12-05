9fb3ee5a6e6745ea7fd0eb83d05042c5ed3c6d81
Jessica <c4006045@newcastle.ac.uk>
2025-12-05 17:59:03 +0000

    part e

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,7 @@ def create_app():
     # session and cookie security
     app.config['SESSION_COOKIE_HTTPONLY'] = True
     app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
-    app.config['SESSION_COOKIE_SECURE'] = False  # True if using HTTPS
+    app.config['SESSION_COOKIE_SECURE'] = False  # true if using HTTPS
 
     db.init_app(app)
     csrf.init_app(app)
@@ -26,6 +26,7 @@ def create_app():
         db.drop_all()
         db.create_all()
 
+        # sample users
         users = [
             {"username": "user1@email.com", "password": "Userpass!23", "role": "user", "bio": "I'm a basic user"},
             {"username": "mod1@email.com", "password": "Modpass!23", "role": "moderator", "bio": "I'm a moderator"},
@@ -36,6 +37,16 @@ def create_app():
             user = User(username=user["username"], password=user["password"], role=user["role"], bio=user["bio"])
             db.session.add(user)
             db.session.commit()
+
+    def apply_security_headers(response):
+        response.headers["X-Frame-Options"] = "DENY" # clickjacking protection
+        response.headers["X-Content-Type-Options"] = "nosniff" # mime sniffling protection
+        response.headers["X-XSS-Protection"] = "1; mode=block" # XSS protection
+        response.headers["Referrer-Policy"] = "strict-origin-when-cross-origin" # referrer privacy
+        response.headers["Permissions-Policy"] = "geolocation=(), microphone=(), camera=(), fullscreen=(self), accelerometer=(), autoplay=(), magnetometer=(), gyroscope=(), payment=()" # permissions policy
+        response.headers["Strict-Transport-Security"] = "max-age=63072000; includeSubDomains; preload" # strict transport security HSTS
+        response.headers["Content-Security-Policy"] = "default-src 'self'; script-src 'self' https://cdn.jsdelivr.net https://cdn.jsdelivr.net/npm/sweetalert2@11 'unsafe-inline'; style-src 'self' https://cdn.jsdelivr.net; img-src 'self' data:; object-src 'none'; base-uri 'self'; frame-ancestors 'none'; form-action 'self'; " # content security policy (CSP)
+        return response
 
     return app
 
--- app/forms.py
+++ app/forms.py
@@ -12,7 +12,7 @@ def username_not_taken(form, field):
     sanitized = username.replace("%", r"\%").replace("_", r"\_")
     user_exists = User.query.filter(User.username.ilike(sanitized)).first()
     if user_exists:
-        raise ValidationError("An account with this email already exists.")
+        raise ValidationError("An account with this email already exists")
 
 # blacklist given in spec
 password_blacklist = {"Password123$", "Qwerty123!", "Adminadmin1@", "weLcome123!"}
@@ -24,7 +24,7 @@ def password_validation(form, field):
 
     # password rules in part B
     if len(password) < 10:
-        raise ValidationError("Password must be at least 10 characters long.")
+        raise ValidationError("Password must be at least 10 characters long")
     if not re.search(r"[A-Z]", field.data):
         raise ValidationError("Password must contain at least 1 uppercase letter")
     if not re.search(r"[0-9]", field.data):
@@ -34,15 +34,15 @@ def password_validation(form, field):
     if username and username.lower() in password.lower():
         raise ValidationError("Password must not contain your username.")
     if re.search(r"(.)\1\1", password):
-        raise ValidationError("Password must not contain repeated repeated sequences (e.g., 'aaa').")
+        raise ValidationError("Password must not contain repeated repeated sequences (e.g., 'aaa')")
     if password in password_blacklist:
-        raise ValidationError("This password is not allowed.")
+        raise ValidationError("This password is not allowed")
 
 # registration form
 class RegisterForm(FlaskForm):
-    username = StringField("Email", validators=[DataRequired(), Email(message="Enter a valid email address."), Length(max=320), username_not_taken])
+    username = StringField("Email", validators=[DataRequired(), Email(message="Enter a valid email address"), Length(max=320), username_not_taken])
     password = PasswordField("Password", validators=[DataRequired(), Length(min=10, max=128), password_validation])
-    confirm = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo('password', message='Passwords must match.')])
+    confirm = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo('password', message='Passwords must match')])
     bio = TextAreaField("Bio", validators=[Optional(), Length(max=2000)])
     submit = SubmitField("Register")
 
